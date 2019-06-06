96fd1b6d20fdcd7486d4125ea0ad2a47ffeefcf6
haidarzxc <haidar.a.alanbari@gmail.com>
2019-06-06 14:12:45 -0400

    zxc

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,7 +14,6 @@ DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
 DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
 
-MEDIA_URL = "media/"
 # CACHES
 # ------------------------------------------------------------------------------
 # CACHES = {
